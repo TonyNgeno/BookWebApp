d837cd22ea1301f877a0d1952e53c02233de09d2
TonyNgeno <tonyngeno123@gmail.com>
2019-02-21 08:13:53 -0800

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBrClEeGv7iItnLS7LPPYyzKwpreZG9RJQ",
+    authDomain: "angular-firebase-97e7d.firebaseapp.com",
+    databaseURL: "https://angular-firebase-97e7d.firebaseio.com",
+    projectId: "angular-firebase-97e7d",
+    storageBucket: "angular-firebase-97e7d.appspot.com",
+    messagingSenderId: "993199650756"
+  } 
+};
